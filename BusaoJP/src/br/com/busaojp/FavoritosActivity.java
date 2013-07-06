d34eda5ebfceff7828faaba9b3f866dcf84f8bb3
Rubens <rubens.jp.br@gmail.com>
2013-07-05 22:23:48 -0300

    novas navegacoes entre telas e inclusao da listviews

--- BusaoJP/src/br/com/busaojp/FavoritosActivity.java
+++ BusaoJP/src/br/com/busaojp/FavoritosActivity.java
@@ -1,28 +1,35 @@
 package br.com.busaojp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import br.com.busaojp.utils.ActivityUtil;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.view.View.OnClickListener;
 
 public class FavoritosActivity extends Activity{
-
+	
+	private ListView mListView;
+	private ArrayAdapter<String> arrayAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.favoritos);
 		
-		Button b = (Button)findViewById(R.id.parada);
-		b.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v) {
-				ActivityUtil.mudarActivity(FavoritosActivity.this, RotasActivity.class);	
-			}
-		});
-
+		mListView = (ListView) findViewById(R.id.lista_favoritos);
+		final String res[] = {"202 - Geisel", "1500 - Circular", "1510 - Circular"};
+		
+		ArrayList<String> lista = new ArrayList<String>();
+		lista.addAll(Arrays.asList(res));
+		arrayAdapter = new ArrayAdapter<String>(this, R.layout.favoritos_listview, lista);
+		mListView.setAdapter(arrayAdapter);
 	}
 
 	@Override
--- BusaoJP/src/br/com/busaojp/ItirenarioActivity.java
+++ BusaoJP/src/br/com/busaojp/ItirenarioActivity.java
@@ -1,17 +1,21 @@
 package br.com.busaojp;
 
+import java.util.ArrayList;
+import br.com.busaojp.utils.ActivityUtil;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
-import br.com.busaojp.utils.ActivityUtil;
 
 public class ItirenarioActivity extends Activity {
-
+	
+	private ListView mListView;
+	private ArrayAdapter<String> arrayAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,21 +27,28 @@ public class ItirenarioActivity extends Activity {
 		if (parametros != null) {
 			String onibus = parametros.getString("linha");
 			TextView textView = (TextView) findViewById(R.id.onibus_title);
-			textView.setText(onibus);
-			
-			
+			textView.setText(onibus);			
 		}
+		
+		mListView = (ListView) findViewById(R.id.lista_rota);		
+		ArrayList<String> lista = new ArrayList<String>();
+		
+		for (int i = 1; i <= 20; ++i) {
+			String s = "Rua número " + i;
+			lista.add(s);
+		}
+		arrayAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, lista);
+		mListView.setAdapter(arrayAdapter);
+		
 	}
 	
-	public void botoes(View v){
-		ImageButton b = (ImageButton) v;
-    	switch (b.getId()) {
-	    	case R.id.mapas: ActivityUtil.mudarActivity(this, RotasActivity.class); break;	
-	    	case R.id.horario: ActivityUtil.mudarActivity(this, HorarioActivity.class); break;
-	    	case R.id.favoritos: ActivityUtil.mudarActivity(this, FavoritosActivity.class); break;	
-	    	default: return;
-    	}
-    }
+	public void mostraHorarios(View v) {
+		ActivityUtil.mudarActivity(this, HorarioActivity.class);
+	}
+	
+	public void verRota(View v) {
+		ActivityUtil.mudarActivity(this, RotasActivity.class);
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
