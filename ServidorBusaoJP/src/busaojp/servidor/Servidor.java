a79cc406ec6b7525b70ef721ffc46a3d53417a54
Rubens <rubens.jp.br@gmail.com>
2013-08-24 12:44:24 -0300

    atualizacao

--- ServidorBusaoJP/src/busaojp/servidor/Servidor.java
+++ ServidorBusaoJP/src/busaojp/servidor/Servidor.java
@@ -16,26 +16,17 @@ import javax.servlet.http.HttpServletResponse;
 public class Servidor extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Servidor() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Requisição recebida.");
 		PrintWriter writer = response.getWriter();
 		writer.println("Olá Mundo!!!");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Requisição recebida.");
 		PrintWriter writer = response.getWriter();
