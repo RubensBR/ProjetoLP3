73e12de2f2fa129e388f608e4def14d6739323ce
Ana Beatrice <anabeatricesevero@gmail.com>
2013-09-01 01:03:10 -0300

    Mexendo nas preferencias

---
+++ BusaoJP/src/br/com/busaojp/BusaoPreferences.java
@@ -0,0 +1,19 @@
+package br.com.busaojp;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class BusaoPreferences extends PreferenceActivity{
+	 
+	
+	@Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	 
+	        addPreferencesFromResource(R.xml.preferencias);
+	        
+	    }
+	}
+	
+
+
--- BusaoJP/src/br/com/busaojp/MainActivity.java
+++ BusaoJP/src/br/com/busaojp/MainActivity.java
@@ -1,25 +1,31 @@
 package br.com.busaojp;
 
-import br.com.busaojp.utils.ActivityUtil;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceManager;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import br.com.busaojp.utils.ActivityUtil;
 
 public class MainActivity extends Activity {
-
+	Resources resources; 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.preferencias, menu);
         return true;
     }
     
@@ -45,6 +51,18 @@ public class MainActivity extends Activity {
 	    	default:
 	    		return;
     	}
+    }
+    
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.config) {
+            ActivityUtil.mudarActivity(this, BusaoPreferences.class);
+            return true;
+        } else if (item.getItemId() == R.id.aboutUs) {
+        	new AlertDialog.Builder(this).setMessage("Este é um projeto desenvolvido para conclusão da disciplina Linguagens de Programação 3 (Android) na UFPB\n\n Equipe: Ana Beatrice Severo\n Carlos André Correia\n Rubens Correia").setTitle("About").setPositiveButton("Voltar", null).show();
+            return true;
+        }
+     
+        return super.onOptionsItemSelected(item);
     }
     
 }
